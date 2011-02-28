e1cbcb4cf0eda8f41027f6ff2004b1153427b1aa
Florent Guilleux <florent2@gmail.com>
2011-02-28 10:05:37 -0500

    Use CamelCase notation

--- application.js
+++ application.js
@@ -1,34 +1,34 @@
 // updates the spans attached to the passed input field to display whether the passed input field content matches the passed regexp value or not
 // input: the text input field
-// regexp_value: the value of the regexp (String)
-function update_result_for(input, regexp_value) {
-  var input_value 	= input.val();
-  var result_spans	= input.parent().children('span');        
-  if(!input_value || !regexp_value) {
-    result_spans.hide();
+// regexpValue: the value of the regexp (String)
+function updateResultFor(input, regexpValue) {
+  var inputValue 	= input.val();
+  var resultSpans	= input.parent().children('span');        
+  if(!inputValue || !regexpValue) {
+    resultSpans.hide();
   } else {
-    var regexp = new RegExp(regexp_value);
-    var result = regexp.exec(input_value);
+    var regexp = new RegExp(regexpValue);
+    var result = regexp.exec(inputValue);
     if(result) {
-      var matched_string = result.shift();
+      var matchedString = result.shift();
       
-      var submatches_list_string = jQuery.map(result, function(submatch, index) {
+      var submatchesListString = jQuery.map(result, function(submatch, index) {
         return '$' + (index + 1) + ' = ' + submatch;
       }).join('; ');
       
-      var regexp_to_highlight_matched_string          = new RegExp('(.*)' + matched_string + '(.*)');
-      var regexp_to_highlight_matched_string_result   = regexp_to_highlight_matched_string.exec(input_value);
-      var before_matched_string                       = regexp_to_highlight_matched_string_result[1];
-      var after_matched_string                        = regexp_to_highlight_matched_string_result[2];
-      var input_value_with_matched_string_highlighted	= 'matched: ' + before_matched_string + '<span class="matched">' + matched_string + '</span>' + after_matched_string;
+      var regexpToHighlightMatchedString          = new RegExp('(.*)' + matchedString + '(.*)');
+      var regexpToHighlightMatchedStringResult   	= regexpToHighlightMatchedString.exec(inputValue);
+      var beforeMatchedString                     = regexpToHighlightMatchedStringResult[1];
+      var afterMatchedString                      = regexpToHighlightMatchedStringResult[2];
+      var inputValueWithMatchedStringHighlighted	= 'matched: ' + beforeMatchedString + '<span class="matched">' + matchedString + '</span>' + afterMatchedString;
       
-      result_spans.filter(".submatches").text(submatches_list_string);
-      result_spans.filter(".match").html(input_value_with_matched_string_highlighted);
-      result_spans.filter(".ok").show('fast');            
-      result_spans.filter(".not_ok").hide();
+      resultSpans.filter(".submatches").text(submatchesListString);
+      resultSpans.filter(".match").html(inputValueWithMatchedStringHighlighted);
+      resultSpans.filter(".ok").show('fast');            
+      resultSpans.filter(".not_ok").hide();
     } else {
-      result_spans.filter(".not_ok").show('fast');
-      result_spans.filter(".ok").hide();
+      resultSpans.filter(".not_ok").show('fast');
+      resultSpans.filter(".ok").hide();
     }
   }
 }       
@@ -49,18 +49,18 @@ $(document).ready(function() {
   $('#regexp').focus();
   $('span.result').hide();
   $('input:not(#regexp)').live("keyup", function() {
-    update_result_for($(this), $('#regexp').val());
+    updateResultFor($(this), $('#regexp').val());
   });        
   $('input#regexp').keyup(function() {
     $('input:not(#regexp)').each(function(i) {
-      update_result_for($(this), $('#regexp').val());
+      updateResultFor($(this), $('#regexp').val());
     });          
   });
   $('a.add_example').click(function() {
-    new_example = $('div#examples p:last').clone();
-    new_example.children('input').attr('value', '');
-    new_example.children('span').hide();
-    new_example.insertBefore($(this));
-    new_example.children("input").focus();
+    newExample = $('div#examples p:last').clone();
+    newExample.children('input').attr('value', '');
+    newExample.children('span').hide();
+    newExample.insertBefore($(this));
+    newExample.children("input").focus();
   });
 });
